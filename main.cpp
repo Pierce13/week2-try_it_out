f7aa115e50562a98cd9fd60e7d2d19e6aadee38d
Pierce13 <piercescott@weber.edu>
2025-11-08 13:39:54 -0700

    init

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    double fahrenheit, celsius;
+    // TODO: Declare two variables that are both doubles, fahrenheit and celsius
 
     cout << "Enter temperature in Fahrenheit: ";
-    cin >> fahrenheit;
+    // TODO: Prompt the user to enter the fahrenheit temperature
 
-    celsius = (fahrenheit - 32) * 5.0 / 9.0;
+    // TODO: Set the celsius variable. The equation is (fahrenheit - 32) * 5.0 / 9.0
 
     cout << "Temperature in Celsius: " << celsius << endl;
 
